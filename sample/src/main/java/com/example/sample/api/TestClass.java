2dc0b39deb21008a1481dfd1e0eb9614104ce3e6
hojin7769 <sungsill150@gmail.com>
2023-01-05 16:12:13 +0900

    q q

---
+++ sample/src/main/java/com/example/sample/api/TestClass.java
@@ -0,0 +1,14 @@
+package com.example.sample.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestClass {
+    @GetMapping("/test")
+    public String testMethod(){
+        return "sss";
+    }
+}
